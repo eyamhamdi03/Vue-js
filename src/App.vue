4ae214c562ce731e7ec456012d268fbab48d8081
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-11-29 18:25:29 +0100

    hero added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import NavBar from './components/Nav-bar.vue'
+import NavBar from '@/components/Nav-bar.vue'
+import HeroSection from '@/components/Hero-section.vue'
 </script>
 <template>
   <NavBar />
+  <HeroSection />
 </template>
