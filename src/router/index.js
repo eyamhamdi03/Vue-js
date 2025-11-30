0bf7ec370f1d87daa57364dfbdf6edff3b290b00
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-11-30 22:07:20 +0100

    not found page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeVue from '@/views/HomeVue.vue'
 import JobsVue from '@/views/JobsVue.vue'
+import NotFoundVue from '@/views/NotFoundVue.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/jobs',
       name: 'jobs',
       component: JobsVue,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFoundVue,
     },
   ],
 })
